8bd57b907155fad7a8cc10c5d69cf8f358656be5
matt_suncy <matt.suncy@gmail.com>
2024-10-21 15:16:33 +0000

    Image to image working now (may need further modification)

--- scripts/pretrained_model_experiments/GALIP_image_to_image.py
+++ scripts/pretrained_model_experiments/GALIP_image_to_image.py
@@ -12,11 +12,11 @@ import clip
 import os.path as osp
 import os, sys
 import torchvision.utils as vutils
+import numpy as np
 
 from src.GALIP.code.lib.utils import load_model_weights,mkdir_p
 from src.GALIP.code.models.GALIP import NetG, CLIP_TXT_ENCODER
 ### 
-# End
 
 if __name__ == "__main__":
 
@@ -37,12 +37,12 @@ if __name__ == "__main__":
     checkpoint = torch.load(path, map_location=torch.device('cpu'))
     netG = load_model_weights(netG, checkpoint['model']['netG'], multi_gpus=False)
 
-    # Generation hyperparams
-    batch_size = 8 # This is literally the number of images it's gonna generate
+    # Generation hyperparameters
+    batch_size = 4 # This is literally the number of images it's gonna generate
     noise = torch.randn((batch_size, 100)).to(device) # Don't really understand why there needs to be noise
 
     # Text prompt
-    captions = ['An overhead shot of a chessboard']
+    captions = ['A kid playing with blocks.']
     
     # CHANGE current directory for 'samples' folder
     this_file_path = os.path.dirname(os.path.abspath(__file__))
@@ -51,7 +51,54 @@ if __name__ == "__main__":
     if not os.path.exists('./samples'):
         os.mkdir('./samples')
 
+    # Load and preprocess the two images, NOTE replace as necessary
+    image1_path = '/home/jovyan/Documents/latplan-temporal-segmentation/videos/frames/kid_playing_with_blocks_1.mp4/0000000000.jpg'  
+    image2_path = '/home/jovyan/Documents/latplan-temporal-segmentation/videos/frames/kid_playing_with_blocks_1.mp4/0000000147.jpg' 
+
+    img1 = Image.open(image1_path)
+    img2 = Image.open(image2_path)
+
+    # Preprocess images using CLIP's preprocess function
+    img1_processed = preprocess(img1).unsqueeze(0).to(device)
+    img2_processed = preprocess(img2).unsqueeze(0).to(device)
+
+    # Extract CLIP embeddings for both images
+    with torch.no_grad():
+        img1_embedding = clip_model.encode_image(img1_processed)
+        img2_embedding = clip_model.encode_image(img2_processed)
+
+    # Number of interpolation steps
+    num_steps = 10  # Adjust as needed for smoother interpolation
+
+    # Interpolate between the two embeddings
+    interpolated_embeddings = []
+    for t in np.linspace(0, 1, num_steps):
+        # Linear interpolation between embeddings
+        embedding_t = (1 - t) * img1_embedding + t * img2_embedding
+        # Normalize the interpolated embedding
+        embedding_t = embedding_t / embedding_t.norm(dim=-1, keepdim=True)
+        interpolated_embeddings.append(embedding_t)
+
     # Image generation pipeline
+    with torch.no_grad():
+        for idx, embedding_t in enumerate(interpolated_embeddings):
+            # Repeat the embedding to match the batch size
+            sent_emb = embedding_t.repeat(batch_size, 1)
+            # Generate images using netG
+            fake_imgs = netG(noise, sent_emb, eval=True).float()
+            # Save the generated images
+            name = f'interpolated_{idx}'
+            vutils.save_image(
+                fake_imgs.data,
+                f'./samples/{name}.png',
+                nrow=8,
+                value_range=(-1, 1),
+                normalize=True
+            )
+
+    # Image generation pipeline
+    # TODO: Modify below to use CLIP to embed images instead of text
+    '''
     with torch.no_grad():
         for i in range(len(captions)):
             caption = captions[i]
@@ -61,3 +108,4 @@ if __name__ == "__main__":
             fake_imgs = netG(noise,sent_emb,eval=True).float()
             name = f'{captions[i].replace(" ", "-")}'
             vutils.save_image(fake_imgs.data, './samples/%s.png'%(name), nrow=8, value_range=(-1, 1), normalize=True)
+    '''
